cdf01a566e70c1aa3e39f2cb4ad4927d3803669f
George Ruck <georgeadruck@gmail.com>
2020-11-19 13:54:27 +0000

    add gitignore

--- dataread.py
+++ dataread.py
@@ -24,6 +24,10 @@ files = [f for f in os.listdir() if f.endswith('.csv')]
 #start with only df group
 df = pd.read_csv(files[1],sep = '\t',encoding = 'utf-16')
 
+#treat time variable
+df['time'] = pd.to_datetime(df['stdate'] + " " + df['sttime'])
+df = df.set_index('time')
+
 ## pre data treatment
 #1. split into locomotion and quantization
 print('Groupby method:')
@@ -31,17 +35,20 @@ t.start()
 df_loc,df_quant = [g for _, g in df.groupby('datatype')]
 t.stop()
 
+#should check performance over the full dataset - the other method is more readable
+"""
 print('Explicit method:')
 t.start()
 df_loc = df[df['datatype']=='Locomotion']
 df_quant = df[df['datatype']=='Quantization']
 t.stop()
+"""
 
 #reorder? - all values present but not all in the correct order
 
 #%%2. visualise nans for one
-isnull = dfs_data[0].isnull().sum()
-sns.heatmap(dfs_data[0].isnull(), yticklabels = False, cbar = False)
+isnull = df_loc.isnull().sum()
+#sns.heatmap(dfs_data[0].isnull(), yticklabels = False, cbar = False)
 
 #3. split into animal type (asign 0 - radix, 1 - gammares,2 - sangsues)
 

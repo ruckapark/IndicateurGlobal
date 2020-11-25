300d0e829e49ce78459904be35bbd7d584ee3c29
George Ruck <georgeadruck@gmail.com>
2020-11-25 14:08:35 +0000

    Get rid of useless columns, formate existing ones, work towards abtime and duration columns to get exact df match.

--- dataread.py
+++ dataread.py
@@ -13,27 +13,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from perftimer import Timer
-t = Timer()
+#from perftimer import Timer
+#t = Timer()
 
 
 os.chdir(r'D:\VP\Viewpoint_data\test')
-print(os.listdir())
-files = [f for f in os.listdir() if f.endswith('.csv')]
 
 #start with only df group
-df = pd.read_csv(files[1],sep = '\t',encoding = 'utf-16')
+df = pd.read_csv('av_dope_2500_764.csv',sep = '\t',encoding = 'utf-16')
+df = df[df['datatype'] == 'Locomotion']
+
+#sort values sn = , pn = ,location = E01-16 etcc., aname = A01-04,B01-04 etc.
+df = df.sort_values(by = ['sn','pn','location','aname'])
 
 #treat time variable
 df['time'] = pd.to_datetime(df['stdate'] + " " + df['sttime'])
 df = df.set_index('time')
 
+#E01 etc.
+mapping = lambda a : {'E': 'Erpobdella','G':'Gammarus','R':'Radix'}[a[0]]
+df['specie'] = df['location'].map(mapping)
+
+#moi le column 'animal' n'a que des NaNs
+good_cols = ['location','stdate','specie','entct','inact','inadur','inadist','smlct','smldur','smldist','larct','lardur','lardist','emptyct','emptydur']
+df = df[good_cols]
+
+#create animal column from location E01
+df['animal'] = df['location'].str[1:].astype(int)
+
+df['dose'] = '72ug/L'
+df['etude'] = 'ETUDE001'
+df['lot'] = 'Zn'
+
+isnull = df.isnull().sum()
+sns.heatmap(df.isnull(), yticklabels = False, cbar = False)
+
+#sml distance all nans? replace with zeros
+df['smldist'] = df['smldist'].fillna(0)
+#%%
+
+#%%
+
 ## pre data treatment
 #1. split into locomotion and quantization
-print('Groupby method:')
-t.start()
-df_loc,df_quant = [g for _, g in df.groupby('datatype')]
-t.stop()
+# print('Groupby method:')
+# t.start()
+# df_loc,df_quant = [g for _, g in df.groupby('datatype')]
+# t.stop()
 
 #should check performance over the full dataset - the other method is more readable
 """
@@ -45,10 +71,6 @@ t.stop()
 """
 
 #reorder? - all values present but not all in the correct order
-
-#%%2. visualise nans for one
-isnull = df_loc.isnull().sum()
-#sns.heatmap(dfs_data[0].isnull(), yticklabels = False, cbar = False)
 
 #3. split into animal type (asign 0 - radix, 1 - gammares,2 - sangsues)
 

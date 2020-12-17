2011ef1ebc1464a88dffa19585159c1294008e1a
George Ruck <georgeadruck@gmail.com>
2020-12-17 16:29:09 +0000

    Created functions for plots, done for first plot. To integrate to dataread import the file as a module and use the functions

--- datagraphs.py
+++ datagraphs.py
@@ -16,6 +16,7 @@ Steps:
 @author: Admin
 """
 
+#%% IMPORTS
 
 import os
 import pandas as pd
@@ -27,10 +28,16 @@ from datetime import timedelta
 from data_merge import merge_dfs
 from dope_reg import dope_read
 
+
+#%% FUNCTIONS
+
+
+#%% SETUP
+
 sns.set_style('darkgrid', {"xtick.major.size": 8, "ytick.major.size": 8})
 sns.set_context('paper')
 
-Tox,species = 763,'G'
+Tox,species = 767,'G'
     
 specie = {'E': 'Erpobdella','G':'Gammarus','R':'Radix'}
 
@@ -117,7 +124,22 @@ df['dist'] = df['inadist'] + df['smldist'] + df['lardist']
 
 #%%
 
-def xticklabel(timestamp,days):
+def dataplot_time():
+    fig = plt.figure(figsize = (20,10), dpi = 100)
+    axe = fig.add_axes([0.1,0.1,0.8,0.8])
+    return fig,axe
+
+def xticklabel_gen(timestamp,lang = 'fr'):
+    """
+    return from timestamp weekday and time HH:MM xtick_label
+    
+    change lang if we want dates in english
+    """
+    if lang == 'fr':
+        days = dict(zip([0,1,2,3,4,5,6],'lu ma me je ve sa di'.split()))
+    else:
+        days = dict(zip([0,1,2,3,4,5,6],'mo tu we th fr sa su'.split()))
+    
     wday = days[timestamp.weekday()]
     hour = timestamp.hour
     minute = timestamp.minute
@@ -133,6 +155,58 @@ def xticklabel(timestamp,days):
         
     return wday + '-' + hour + ':' + minute
 
+def dataplot_time_params(axe,title,xticks,ylab = None,xlab = None,
+                         ylim = None,xlim = None, rot = 10
+                         ):
+    axe.set_title(title)
+    axe.legend()
+    axe.set_ylabel(ylab)
+    axe.set_xlabel(xlab)
+    if ylim: axe.set_ylim(ylim[0],ylim[1])
+    if xlim: axe.set_xlim(xlim[0],xlim[1])    
+    axe.set_xticks(xticks)
+    axe.set_xticklabels([xticklabel_gen(i) for i in xticks])
+    plt.xticks(rotation = rot)
+    
+def dataplot_mark_dopage(axe,dope_df,date,Tox):
+    """
+    Shade the doping period and mark the doping moment thoroughly
+    """
+    date_range = [
+    dope_df[(dope_df['TxM'] == Tox) & (dope_df['Start'].dt.strftime('%d/%m/%Y') == date_dopage)]['Start'],
+    dope_df[(dope_df['TxM'] == Tox) & (dope_df['End'].dt.strftime('%d/%m/%Y') == date_dopage)]['End']
+    ]
+    
+    #shade over doping period - item extracts value from pandas series
+    axe.axvspan(date_range[0].item(), date_range[1].item(), alpha=0.7, color='orange')
+    
+    #plot vertical line at estimated moment of dopage
+    axe.axvline(date_range[0].item() + pd.Timedelta(minutes = 1), color = 'red')
+    
+    #could add possibility to put xtick in location of doping?
+
+    
+
+fig,axe = dataplot_time()
+for i in range(1,17):
+    axe.plot(df[df['animal']==i]['time'], df[df['animal']==i]['dist'], label = i)
+
+no_xticks = 10
+xticks = [df.iloc[i*len(df)//no_xticks]['time'] for i in range(no_xticks)]
+
+dataplot_time_params(
+    axe,
+    '20s distance - {}'.format(specie[species]),
+    xticks,
+    ylab = 'Distance',
+    ylim = [0,df['dist'].quantile(0.9999)]
+    )
+
+dope_df = dope_read()
+date_dopage = dfs[-1].iloc[0]['time'].strftime('%d/%m/%Y')
+dataplot_mark_dopage(axe,dope_df,date_dopage,Tox)
+
+
 #plot all animals
 fig = plt.figure(figsize = (20,10), dpi = 100)
 axe = fig.add_axes([0.1,0.1,0.8,0.8])
@@ -144,29 +218,29 @@ axe.set_ylabel('Distance')
 axe.set_xlabel('Date (continuous adjustment)')
 axe.set_ylim(0,df['dist'].quantile(0.9999))
 
-# set xticklabels to day of week
-days = dict(zip([0,1,2,3,4,5,6],'lu ma me je ve sa di'.split()))
+# set xticklabels with certain number of xticks for days of the week
 no_xticks = 10
 xticks = [df.iloc[i*len(df)//no_xticks]['time'] for i in range(no_xticks)]
 axe.set_xticks(xticks)
-axe.set_xticklabels([xticklabel(i,days) for i in xticks])
+axe.set_xticklabels([xticklabel_gen(i) for i in xticks])
 plt.xticks(rotation = 10)
-
 
 #shade in doping in orange with a red line and the 40-60 seconds after interval
 dope_df = dope_read()
-date = dfs[-1].iloc[0]['time'].strftime('%d/%m/%Y')
+date_dopage = dfs[-1].iloc[0]['time'].strftime('%d/%m/%Y')
+
+#dataplot_mark_dopage(dope_df,datedopage)
 
 ##  DEBUG - occuring because 04/12/2020 -> 00:00:00. Therefore it happens again.
 date_range = [
-    dope_df[(dope_df['TxM'] == Tox) & (dope_df['Start'].dt.strftime('%d/%m/%Y') == date)]['Start'],
-    dope_df[(dope_df['TxM'] == Tox) & (dope_df['End'].dt.strftime('%d/%m/%Y') == date)]['End']
+    dope_df[(dope_df['TxM'] == Tox) & (dope_df['Start'].dt.strftime('%d/%m/%Y') == date_dopage)]['Start'],
+    dope_df[(dope_df['TxM'] == Tox) & (dope_df['End'].dt.strftime('%d/%m/%Y') == date_dopage)]['End']
     ]
 
-#column tox = tox, date = date
+#shade over doping period - item extracts value from pandas series
 axe.axvspan(date_range[0].item(), date_range[1].item(), alpha=0.7, color='orange')
 
-#assume it is around 60 seconds after I note.
+#plot vertical line at estimated moment of dopage
 axe.axvline(date_range[0].item() + pd.Timedelta(minutes = 1), color = 'red')
 
 #%%

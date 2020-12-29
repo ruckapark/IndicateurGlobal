07e05f5ba52b464c9fab386c41fb38f4d57acde0
George Ruck <georgeadruck@gmail.com>
2020-12-29 12:03:54 +0000

    Original IGT graphs added, now to start pushing a bit further into the IGT - merge branch to master

--- datagraphs.py
+++ datagraphs.py
@@ -36,15 +36,24 @@ from dope_reg import dope_read
 
 sns.set_style('darkgrid', {"xtick.major.size": 8, "ytick.major.size": 8})
 sns.set_context('paper')
+colors = [
+    '#42f5e0','#12aefc','#1612fc','#6a00a3',
+    '#8ef743','#3c8f01','#0a4001','#fc03ca',
+    '#d9d200','#d96c00','#942c00','#fc2803',
+    '#e089b6','#a3a3a3','#7a7a7a','#303030'
+    ]
 
 # Tox,species = 767,'G'
 
 def main(Tox, species = 'G'):        
+    #Tox,species = 767,'G'
     specie = {'E': 'Erpobdella','G':'Gammarus','R':'Radix'}
     
     #os.chdir(r'D:\VP\Viewpoint_data\TxM{}-PC'.format(Tox))
     os.chdir(r'D:\VP\Viewpoint_data\TxM{}-PC'.format(Tox))
-    files = os.listdir()
+    files = [file for file in os.listdir() if os.path.isfile(file)]
+    if 'Results' not in os.listdir():
+        os.mkdir('Results')
     
     print('The following files will be merged:')
     print(files)
@@ -122,7 +131,6 @@ def main(Tox, species = 'G'):
     #%% FUNCTIONS
     
     
-    
     def dataplot_time():
             fig = plt.figure(figsize = (20,10), dpi = 100)
             axe = fig.add_axes([0.1,0.1,0.8,0.8])
@@ -155,10 +163,10 @@ def main(Tox, species = 'G'):
             return wday + '-' + hour + ':' + minute
     
     def dataplot_time_params(axe,title,xticks,ylab = None,xlab = None,
-                                 ylim = None,xlim = None, rot = 10
+                                 ylim = None,xlim = None, rot = 10, text = None
                                  ):
             axe.set_title(title)
-            axe.legend()
+            axe.legend()            #if no label pas grave
             axe.set_ylabel(ylab)
             axe.set_xlabel(xlab)
             if ylim: axe.set_ylim(ylim[0],ylim[1])
@@ -166,21 +174,19 @@ def main(Tox, species = 'G'):
             axe.set_xticks(xticks)
             axe.set_xticklabels([xticklabel_gen(i) for i in xticks])
             plt.xticks(rotation = rot)
+            # doesnt work
+            if text: axe.text(0.75,0.75,text,fontsize = 9, bbox = dict(boxstyle = 'round', alpha = 0.6))
         
-    def dataplot_mark_dopage(axe,dope_df,date,Tox):
+    def dataplot_mark_dopage(axe,date_range):
             """
             Shade the doping period and mark the doping moment thoroughly
             """
-            date_range = [
-            dope_df[(dope_df['TxM'] == Tox) & (dope_df['Start'].dt.strftime('%d/%m/%Y') == date)]['Start'],
-            dope_df[(dope_df['TxM'] == Tox) & (dope_df['End'].dt.strftime('%d/%m/%Y') == date)]['End']
-            ]
-            
             #shade over doping period - item extracts value from pandas series
-            axe.axvspan(date_range[0].item(), date_range[1].item(), alpha=0.7, color='orange')
+            axe.axvspan(date_range[0], date_range[1], alpha=0.7, color='orange')
             
             #plot vertical line at estimated moment of dopage
-            axe.axvline(date_range[0].item() + pd.Timedelta(minutes = 2), color = 'red')
+            #could add dopage as function parameter
+            axe.axvline(date_range[0] + pd.Timedelta(seconds = 90), color = 'red')
         
         #could add possibility to put xtick in location of doping?
     
@@ -190,27 +196,40 @@ def main(Tox, species = 'G'):
         df[df['dist']>0]['dist'].hist(bins = 1000)
         plt.axvline(x = 200, color = 'red')
         plt.title('Valeurs nulles: {}%, Valeurs 1-200: {}%, Valeurs 200+: {}%'.format(int(100*len(df[(df.dist == 0)])/len(df)),int(100*len(df[(df.dist > 0) & (df.dist < 200)])/len(df)),int(100*len(df[df.dist > 200])/len(df))))
+        
+    def savefig(name, fig):
+        os.chdir('Results')
+        fig.savefig(name)
+        os.chdir('..')
     
     def dope_params(df, Tox, date):
         
+        date_range = [
+            df[(df['TxM'] == Tox) & (df['Start'].dt.strftime('%d/%m/%Y') == date)]['Start'].values[0],
+            df[(df['TxM'] == Tox) & (df['End'].dt.strftime('%d/%m/%Y') == date)]['End'].values[0]
+            ]
+        dopage = date_range[0] + pd.Timedelta(seconds = 90)
+        
         # row value of experiment in dope reg
-        conc = df[(dope_df['TxM'] == Tox) & (dope_df['Start'].dt.strftime('%d/%m/%Y') == date)]['Concentration']
-        sub = df[(dope_df['TxM'] == Tox) & (dope_df['Start'].dt.strftime('%d/%m/%Y') == date)]['Substance']
-        molecule = df[(dope_df['TxM'] == Tox) & (dope_df['Start'].dt.strftime('%d/%m/%Y') == date)]['Molecule']
+        conc = df[(dope_df['TxM'] == Tox) & (dope_df['Start'].dt.strftime('%d/%m/%Y') == date)]['Concentration'].values[0]
+        sub = df[(dope_df['TxM'] == Tox) & (dope_df['Start'].dt.strftime('%d/%m/%Y') == date)]['Substance'].values[0]
+        molecule = df[(dope_df['TxM'] == Tox) & (dope_df['Start'].dt.strftime('%d/%m/%Y') == date)]['Molecule'].values[0]
         
         """
         etude is the number of the week of the experiment.
         Etude1 would be my first week of experiments
         """
         etude = df[(dope_df['TxM'] == Tox) & (dope_df['Start'].dt.strftime('%d/%m/%Y') == date)].index[0]//5 + 1
-        return conc,sub,molecule,etude
+        return dopage,date_range,conc,sub,molecule,etude
         
     
     #%% plot
         
     date_dopage = dfs[-1].iloc[0]['time'].strftime('%d/%m/%Y')
     dope_df = dope_read()
-    conc,sub,molecule,etude = dope_params(dope_df,Tox,date_dopage)
+    
+    # dopage is exact date of dopage
+    dopage,date_range,conc,sub,molecule,etude = dope_params(dope_df,Tox,date_dopage)
     
     df['dose'] = conc
     df['etude'] = 'ETUDE{}'.format(etude)
@@ -225,7 +244,7 @@ def main(Tox, species = 'G'):
     
     fig,axe = dataplot_time()
     for i in range(1,17):
-        axe.plot(df[df['animal']==i]['time'], df[df['animal']==i]['dist'], label = i)
+        axe.plot(df[df['animal']==i]['time'], df[df['animal']==i]['dist'], label = i, color = colors[i-1])
     
     no_xticks = 10
     xticks = [df.iloc[i*len(df)//no_xticks]['time'] for i in range(no_xticks)]
@@ -235,10 +254,11 @@ def main(Tox, species = 'G'):
         '20s distance - {}'.format(specie[species]),
         xticks,
         ylab = 'Distance',
-        ylim = [0,df['dist'].quantile(0.9999)]
+        ylim = [0,df['dist'].quantile(0.9999)],
+        text = '{} \n{} \n{}'.format(sub,conc,molecule)
         )
     
-    dataplot_mark_dopage(axe,dope_df,date_dopage,Tox)
+    dataplot_mark_dopage(axe,date_range)
     
     
     #%% generate
@@ -247,7 +267,7 @@ def main(Tox, species = 'G'):
     """
     
     # this does not work
-    timestep_min = 1
+    timestep_min = 10
     timestep = timestep_min*60 # seconds
     df['timestep'] = df['abtime']//timestep * timestep
     
@@ -261,6 +281,7 @@ def main(Tox, species = 'G'):
     df_mean_dist = pd.DataFrame(index = timestamps)
     
     #groupby animal method? - create df with column as cell and mean distances
+    #by making this from the means, the cadre mean and IGT is calculated from a mean ...
     for i in animals:
         temp_df = df[df['animal'] == i]
         mean_distance = temp_df.groupby(['timestep']).mean()['dist']
@@ -274,7 +295,7 @@ def main(Tox, species = 'G'):
     
     fig_means,axe_means = dataplot_time()
     for i in range(1,17):
-        axe_means.plot(df_mean_dist.index,df_mean_dist[i], label = i)
+        axe_means.plot(df_mean_dist.index,df_mean_dist[i], label = i, color = colors[i-1])
     
     no_xticks = 10
     xticks = [df.iloc[i*len(df)//no_xticks]['time'] for i in range(no_xticks)]
@@ -286,7 +307,7 @@ def main(Tox, species = 'G'):
         ylab = 'Distance'
         )
     
-    dataplot_mark_dopage(axe_means,dope_df,date_dopage,Tox)
+    dataplot_mark_dopage(axe_means,date_range)
     
     #%% calculate
     """
@@ -306,21 +327,74 @@ def main(Tox, species = 'G'):
     
     fig_mean_all,axe_mean_all = dataplot_time()
     axe_mean_all.plot(mean_dist.index,mean_dist)
+    
     dataplot_time_params(
         axe_mean_all,
         '{} - Mean cadre'.format(specie[species]),
         xticks,
         ylab = 'Distance')
-    dataplot_mark_dopage(axe_mean_all,dope_df,date_dopage,Tox)
+    dataplot_mark_dopage(axe_mean_all,date_range)
+    
+    #############
     
     fig_IGT,axe_IGT = dataplot_time()
     axe_IGT.plot(quantile_dist.index,quantile_dist)
     dataplot_time_params(
         axe_IGT,
-        '{} - IGT'.format(specie[species]),
+        '{} - IGT    {}   {}   {}'.format(specie[species],sub,conc,etude),
         xticks,
         ylab = 'IGT')
-    dataplot_mark_dopage(axe_IGT,dope_df,date_dopage,Tox)
+    dataplot_mark_dopage(axe_IGT,date_range)
     
-for i in range(763,767):
-    main(i)
+    savefig('{}_IGT.jpg'.format(species), fig_IGT)
+    
+    ############
+    
+    """
+    IGT plot for 10 hours - (-2 from dope time, + 8 from dope time)
+    
+    subplot ()
+    """
+    start10 = dopage - pd.Timedelta(hours = 2)
+    print(start10)
+    print(dopage)
+    end10 = start10 + pd.Timedelta(hours = 10)
+    #extract df values for 10 hours
+    quantile_dist10 = quantile_dist[(quantile_dist.index > start10) & (quantile_dist.index < end10)]
+    
+    fig_IGT10,axe_IGT10 = dataplot_time()
+    axe_IGT10.plot(quantile_dist10.index,quantile_dist10)
+    dataplot_time_params(
+        axe_IGT10,
+        '{} - IGT10hr    {}   {}   {}'.format(specie[species],sub,conc,etude),
+        xticks,
+        ylab = 'IGT')
+    dataplot_mark_dopage(axe_IGT10,date_range)
+    
+    savefig('{}_IGT10.jpg'.format(species), fig_IGT10)
+    
+    """
+    IGT plot for 3 hours - (-0.5 + 2.5)
+    """
+    
+    start3 =  dopage - pd.Timedelta(minutes = 30)
+    end3 = start3 + pd.Timedelta(hours = 3)
+    #extract df values for 3 hours
+    quantile_dist3 = quantile_dist[(quantile_dist.index > start3) & (quantile_dist.index < end3)]
+    
+    fig_IGT3,axe_IGT3 = dataplot_time()
+    axe_IGT3.plot(quantile_dist3.index,quantile_dist3)
+    dataplot_time_params(
+        axe_IGT3,
+        '{} - IGT3hr    {}   {}   {}'.format(specie[species],sub,conc,etude),
+        xticks,
+        ylab = 'IGT')
+    dataplot_mark_dopage(axe_IGT3,date_range)
+    
+    savefig('{}_IGT3.jpg'.format(species), fig_IGT3)
+    
+#main(767,'E')
+
+for i in range(763,768):
+    for spec in ['G','E','R']:
+        main(i,spec)

865e7581725f35d614ff008b89e0aca608446015
Ruckapark <georgeadruck@gmail.com>
2023-06-20 15:45:01 +0200

    Test correlation time lagged (TLCC)

--- LAB_testREPLAY.py
+++ LAB_testREPLAY.py
@@ -34,8 +34,24 @@ roots = ['765_20211022',
          '765_20220310',
          '765_20220317']
 
+def TLCC(df1,df2):
+    """ Time lagged cross correlation - expecting high positive value """
+    return None
+
+def ccf_values(series1, series2):
+    p = series1
+    q = series2
+    p = (p - np.mean(p)) / (np.std(p) * len(p))
+    q = (q - np.mean(q)) / (np.std(q))  
+    c = np.correlate(p, q, 'full')
+    return c
 
 #%% Code
+
+"""
+% of time higher then the other
+average difference between values 
+"""
 
 if __name__ == '__main__':
     
@@ -58,8 +74,34 @@ if __name__ == '__main__':
     #register
     #dope_df = dope_read_extend()
     
-    #%%
+    #%% Start with Radix
+    
     for i in range(1,17):
         plt.figure()
         plt.plot(np.arange(dfs_og['R'].shape[0]),dfs_og['R'][i])
         plt.plot(np.arange(dfs_copy['R'].shape[0]),dfs_copy['R'][i])
+        
+        
+    #%% Radix in first cage
+    df1,df2 = dfs_og['R'],dfs_copy['R']
+    indexing = min(df1.shape[0],df2.shape[0])
+    df1,df2 = df1.iloc[:indexing],df2.iloc[:indexing]
+    
+    #%% check for TLCC (time lag cross correlation)
+    
+    #extract 50 point series from copies - assume movement at beginning of distribution
+    ti = 50
+    #what if dead organism (error?)
+    while True:
+        serie1 = df1[1].iloc[ti:ti+200]
+        if np.sum(serie1<1) > 15:
+            ti += 100
+        else:
+            break
+    serie2 = df2[1].iloc[ti:ti+50]
+    
+    c = np.correlate(serie1, serie2, 'full')
+    
+    from scipy import signal
+    lags = signal.correlation_lags(len(serie1), len(serie2))
+    #no normalisation necessary

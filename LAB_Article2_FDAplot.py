cfe9fd3ea06edae82e08d6a6cb500557260e863e
Ruckapark <georgeadruck@gmail.com>
2023-12-20 22:52:23 +0100

    Begin FP plot and attempt ellipse and metric distance plot

--- LAB_Article2_FDAplot.py
+++ LAB_Article2_FDAplot.py
@@ -12,9 +12,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+from scipy.spatial import ConvexHull, convex_hull_plot_2d
 
-def elipse():
-    
+def ellipse():
     return None
     
 def vertical_PC_plot():
@@ -24,6 +24,8 @@ def vertical_PC_plot():
 if __name__ == "__main__":
     
     #simulate data
+    substances = ['Copper','Methomyl','Verapamil','Zinc']
+    counts = {s:3 for s in substances}
     plot_colors = {'Copper':'#8c564b','Methomyl':'#9467bd','Verapamil':'#d62728','Zinc':'#e377c2'}
     custom_palette = [plot_colors[s] for s in plot_colors]
     sns.set_palette(custom_palette)
@@ -35,16 +37,26 @@ if __name__ == "__main__":
         [-0.2,0.4],[-0.5,0.3],[-0.32,0.37],
         [0.4,-0.2],[0.45,-0.17],[0.41,-0.21]])
     
+    sub_scores = {}
+    for i,s in enumerate(counts):
+        sub_scores.update({s:scores[3*i:3*(i+1)]})
+    
     fig,ax = plt.subplots(figsize=(6, 4))
     sns.scatterplot(x=scores[:, 0], y=scores[:, 1],hue = y,ax = ax)
     ax.set_xlabel("fPC 1 score")
     ax.set_ylabel("fPC 2 score")
     ax.set_title("FPCA scores Gammarus means")
     
+    plot_smallest_enclosing_ellipse(sub_scores['Copper'], ax,color = plot_colors['Copper'])
+    
     ax.tick_params(labelsize = 13)
     
     fig,ax = plt.subplots(figsize=(6, 4))
     sns.scatterplot(x=np.zeros(scores.shape[0]), y=scores[:, 0],hue = y,ax = ax)
     ax.set_title("FPC1 scores")
+    ax.tick_params(labelsize = 13)
     
+    fig,ax = plt.subplots(figsize=(6, 4))
+    sns.scatterplot(x=np.zeros(scores.shape[0]), y=scores[:, 1],hue = y,ax = ax)
+    ax.set_title("FPC2 scores")
     ax.tick_params(labelsize = 13)

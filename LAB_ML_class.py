7e5262eb545aa9cf4532c36576db0a7959d4adfb
Ruckapark <georgeadruck@gmail.com>
2024-01-23 14:41:17 +0100

    Optimise clusters and compare to TyPol and Molecule type Classes

--- LAB_ML_class.py
+++ LAB_ML_class.py
@@ -14,6 +14,50 @@ import seaborn as sns
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances
 
+from matplotlib.lines import Line2D
+
+def index_dictionary(y, unique_elements):
+    index_dict = {}
+
+    for elem in unique_elements:
+        indices = [i for i, x in enumerate(y) if x == elem]
+        index_dict[elem] = indices
+
+    return index_dict
+
+# def cluster_sparsity(cluster_centers, scores, index, dim = 2):
+#     array = scores[:,:dim]
+#     cluster_points = {s:}
+#     sparsity_dict = {}
+
+#     for cluster, center in cluster_centers.items():
+#         center_np = np.array(center)
+#         points_np = np.array(surrounding_points[cluster])
+
+#         # Transpose the points if needed (to have shape (num_p, 2))
+#         if points_np.shape[1] != point_dim:
+#             points_np = np.transpose(points_np)
+
+#         # Calculate distances from the cluster center
+#         distances = np.linalg.norm(points_np - center_np, axis=1)
+
+#         # Normalize distances by the absolute distance of the cluster center from 0,0
+#         normalized_distances = distances / np.linalg.norm(center_np)
+
+#         # Calculate the average normalized distance (cluster sparsity)
+#         sparsity = np.mean(normalized_distances)
+#         sparsity_dict[cluster] = sparsity
+
+#     return sparsity_dict
+
+# # Example usage
+# cluster_centers = {'a': [0, 1], 'b': [2, 2]}
+# surrounding_points = {'a': [[0, 1], [1, 1.3], [0, 1], [-1, 0.7]], 'b': [[2, 2], [2.5, 2.7], [3, 2.8], [2, 2.9]]}
+
+# result_dict = cluster_sparsity(cluster_centers, surrounding_points)
+# print(result_dict)
+
+
 class Scores_data:
     
     def __init__(self,data,y):
@@ -26,20 +70,44 @@ class Scores_data:
             array of list of labels.
         """
         self.data = data
+        self.data_ = self.scale()
         self.y = y
         self.labels = y
+        
+    def scale(self,method = 'log1p'):
+        
+        #Adapt this to only treat one column at a time
+        if method == 'log1p':
+            return self.log1p_scale(self.data)
+        elif method == 'log':
+            return self.log_scale(self.data)
+        elif method == 'sqrt1p':
+            return self.sqrt1p_scale(self.data)
+        elif method == 'sqrt':
+            return self.sqrt_scale(self.data)
+        else:
+            print('Method Unknown')
         
     def scoreplot(self,fPC_x = 1,fPC_y = 2,y = None):
         
         if not y: y = self.y
-        x_,y_ = self.data[:,fPC_x-1],self.data[:,fPC_y-1]
-        fig,ax = plt.subplots(figsize=(18, 16))
+        x_,y_ = self.data_[:,fPC_x-1],self.data_[:,fPC_y-1]
+        fig,ax = plt.subplots(figsize=(16, 14))
         sns.scatterplot(x=x_, y=y_,hue = y,ax = ax,legend = True)
         ax.set_xlabel("fPC 1 score")
         ax.set_ylabel("fPC 2 score")
         ax.set_title("FPCA scores - method: {}".format(method))
         
-        ax_FPCA.tick_params(labelsize = 10)
+        ax.tick_params(labelsize = 10)
+        
+    def log1p_scale(self,array):
+        return np.sign(array) * np.log1p(np.abs(array))
+    def log_scale(self,array):
+        return np.sign(array) * np.log(np.abs(array))
+    def sqrt1p_scale(self,array):
+        return np.sign(array) * (np.sqrt(np.abs(array) + np.ones(shape = array.shape)) - np.ones(shape = array.shape))
+    def sqrt_scale(self,array):
+        return np.sign(array) * np.sqrt(np.abs(array))
 
 class ML_FDA:
     
@@ -47,6 +115,8 @@ class ML_FDA:
         parameter = None
 
 if __name__ == '__main__':
+    
+    plt.close('all')
     
     #Either multidimensional by default or by species
     method = 'Default'
@@ -62,7 +132,13 @@ if __name__ == '__main__':
     
     #Get associated explained variance and scores
     variance = np.array(df_variance.loc[method])
-    scores = df_scores[df_scores.columns[1:]].values
+    scores_class = Scores_data(df_scores[df_scores.columns[1:]].values, y)
+    
+    scores = scores_class.data_
+    y = scores_class.y
+    substances = pd.Series(y).unique()
+    
+    index_y = index_dictionary(y, substances)
     
     #Plot fPCA Score Plot
     x_,y_ = scores[:,0],scores[:,1]
@@ -71,8 +147,99 @@ if __name__ == '__main__':
     ax_FPCA.set_xlabel("fPC 1 score")
     ax_FPCA.set_ylabel("fPC 2 score")
     ax_FPCA.set_title("FPCA scores - method: {}".format(method))
+    ax_FPCA.legend(loc='center left', bbox_to_anchor=(1, 0.5),fontsize = 9)
     
     ax_FPCA.tick_params(labelsize = 13)
+    fig_FPCA.tight_layout()
+    
+    # Create a custom palette with five symbols repeated for every ten labels
+    plot_params = {s:['o','#aaaaaa'] for s in substances}
+    symbols = ['o', 's', 'd', 'X', '^']
+    custom_palette = ['#386cb0', '#fdb462', '#7fc97f', '#d73027', '#66c2a5', '#e34a33', '#b2df8a', '#8856a7', '#fdae61']
+    custom_palette = ['#386cb0', '#e3c956', '#548c54', '#d73027', '#66c2a5', '#e333a8', '#b2df8a', '#8856a7', '#fdae61']
+    for i,s in enumerate(substances):
+        marker = symbols[i//len(custom_palette)]
+        color = custom_palette[i%len(custom_palette)]        
+        plot_params[s] = [marker,color]
+    
+    #Replot with substance specific marker
+    x_,y_ = scores[:,0],scores[:,1]
+    fig_FPCA,ax_FPCA = plt.subplots(figsize=(16, 14))
+    for i in range(scores.shape[0]):
+        s = y[i]
+        ax_FPCA.scatter(scores[i,0],scores[i,1],marker = plot_params[s][0],color = plot_params[s][1],s = 25)
+        
+    #Plot legend
+    handles,labels = [],[]
+    for label, (marker, color) in plot_params.items():
+        # Create a Line2D object with the specified marker and color
+        handle = Line2D([0], [0], marker=marker, color=color, markersize=8, label=label)
+        handles.append(handle)
+        labels.append(label)
+        
+    ax_FPCA.legend(handles=handles, labels=labels,loc='center left', bbox_to_anchor=(1, 0.5),fontsize = 8)
+    
+    #%%read in molecules class
+    molecules = pd.read_csv(r'D:\VP\Viewpoint_data\REGS\molecules.csv',index_col = 0)
+    y_class = [molecules.loc[y[i]]['Class'] for i in range(len(y))]
+    y_typol = ['Class {}'.format(int(molecules.loc[y[i]]['TyPol'])) for i in range(len(y))]
+    y_typol2 = [x for x in y_typol]
+    for i in range(len(y_typol2)):
+        if y_typol2[i] == 'Class 4':
+            y_typol2[i] = 'Class 3'
+        elif y_typol2[i] == 'Class 5':
+            y_typol2[i] = 'Class 4'
+    
+    scores_class.scoreplot(y = y_class)
+    scores_class.scoreplot(y = y_typol)
+    
+    #Calculate molecule cluster centres
+    centres = np.zeros((len(substances),scores.shape[1]))
+    for i,s in enumerate(substances):
+        centres[i] = np.mean(scores[index_y[s]],axis = 0)
+        
+    cluster_centres = {s:[centres[i][0],centres[i][1]] for i,s in enumerate(substances)}
+    
+    #centre sparsity
+    dim = 2
+    array = scores[:,:dim]
+    cluster_points = {s:array[index_y[s]] for s in substances}
+    sparsity_dict = {}
+
+    for cluster, centre in cluster_centres.items():
+        centre_np = np.array(centre)
+        points_np = np.array(cluster_points[cluster])
+
+        # Calculate distances from the cluster center
+        distances = np.linalg.norm(points_np - centre_np, axis=1)
+
+        # Normalize distances by the absolute distance of the cluster center from 0,0
+        normalized_distances = distances / np.linalg.norm(centre_np)
+
+        # Calculate the average normalized distance (cluster sparsity)
+        sparsity = np.mean(normalized_distances)
+        sparsity_dict[cluster] = sparsity
+    
+    
+    # Sort the dictionary by values in ascending order
+    sorted_dict = dict(sorted(sparsity_dict.items(), key=lambda item: item[1]))
+    
+    # Create a bar plot using Seaborn
+    fig,ax = plt.subplots(figsize=(16, 14))
+    sns.barplot(x=list(sorted_dict.keys()), y=list(sorted_dict.values()),ax = ax)
+    
+    # Set labels and title
+    ax.set_xlabel('Cluster')
+    ax.set_ylabel('Cluster Sparsity')
+    ax.set_title('Cluster Sparsity in Ascending Order')
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=90)  # Rotate x-axis labels
+    
+    ax.set_yscale('log')
+    
+    # Show the plot
+    plt.show()
+    
+    
     
     #%%perform k_means on scores dataset
     
